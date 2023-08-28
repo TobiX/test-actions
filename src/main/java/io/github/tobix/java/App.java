ca511071cfea974427359fafaa158cf1b8d5543a
Tobias Gruetzmacher <tobias-git@23.gs>
2023-08-29 01:47:47 +0200

    Spotless all the things!

--- src/main/java/io/github/tobix/java/App.java
+++ src/main/java/io/github/tobix/java/App.java
@@ -1,13 +1,8 @@
 package io.github.tobix.java;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+/** Hello world! */
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
--- src/test/java/io/github/tobix/java/AppTest.java
+++ src/test/java/io/github/tobix/java/AppTest.java
@@ -4,17 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+/** Unit test for simple App. */
+class AppTest {
+  /** Rigorous Test :-) */
+  @Test
+  void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
 }
